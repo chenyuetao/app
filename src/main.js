665d0b6c7f796236f5a443c701de594db5bd3443
chenyuetao <1003228514@qq.com>
2022-05-07 17:30:11 +0800

    merge

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import { Button, Container, Header, Aside, Main, Footer } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container style="height: 100%">
+    <el-aside width="auto">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.el-header {
+  background: #333;
+}
+</style>
