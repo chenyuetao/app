7727b9f66d28c7e8ee98cdeb805868971ec0ba76
ytchen <ytchen@example.com>
2022-05-20 10:20:48 +0000

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,45 @@ Vue.use(VueRouter)
 const routes = [
     {
         path: '/',
+        redirect: '/login'
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/login')
+    },
+    {
+        path: '/home',
         home: 'Home',
         component: () => import('@/views/Home')
     }
 ]
 
-export default new VueRouter({
+const router = new VueRouter({
     mode: 'history',
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { x: 0, y: 0 }
+        }
+    }
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') {
+        next()
+    } else {
+        let token = localStorage.getItem('token')
+        if (!token) {
+            //全局提示
+
+            next('/login')
+        } else {
+            
+        }
+    }
+})
+
+export default router
